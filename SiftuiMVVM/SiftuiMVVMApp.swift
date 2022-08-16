d04164871fe47a81a4d74d317521aa1b0585cde0
co.abu <anizomxoja@gmail.com>
2022-08-16 10:47:58 +0500

    Initial Commit

---
+++ SiftuiMVVM/SiftuiMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiftuiMVVMApp.swift
+//  SiftuiMVVM
+//
+//  Created by Avaz Mukhitdinov on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SiftuiMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
